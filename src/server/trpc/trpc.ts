58981f5bb3c6a5b9937daddd80818b22aabb56d0
Ryan Hopper-Lowe <rhopperlowe@gmail.com>
2024-01-09 20:16:26 -0600

    add 1 second delay to dev apis

--- src/server/trpc/trpc.ts
+++ src/server/trpc/trpc.ts
@@ -33,8 +33,8 @@ export const authorizedProcedure = authedProcedure.use(async (opts) => {
   });
 
   if (process.env.NODE_ENV === "development") {
-    // wait 2 seconds to simulate a slow connection
-    await new Promise((resolve) => setTimeout(resolve, 2000));
+    // wait 1 second to simulate a slow connection
+    await new Promise((resolve) => setTimeout(resolve, 1000));
   }
 
   if (!profile) {
